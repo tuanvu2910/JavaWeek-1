41dde4f2467d60f35e73a9bcdf97e83a93fb133f
tuanvu2910 <phantuanvu56789@gmail.com>
2019-10-21 15:33:57 +0700

    mouth

---
+++ src/com/company/Showloichao.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Showloichao {
+    public static void main(String[] args) {
+     String hey = "";
+        Scanner Hello = new Scanner(System.in);
+     System.out.print("Your name");
+     hey = Hello.nextLine();
+     System.out.printf("Your name is %d",hey);
+
+}
+}
