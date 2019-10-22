faf5521d1f3e26ce15aa41336f97a4ec6c7817db
tuanvu2910 <phantuanvu56789@gmail.com>
2019-10-22 15:30:19 +0700

    daff

---
+++ src/com/company/DesignMenu.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DesignMenu {
+    public static void main(String[] args) {
+        int Choice;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1.Draw the triangle");
+        System.out.println("2.Draw the square ");
+        System.out.println("3.Draw the rectangle");
+        System.out.println("0.Exit");
+        System.out.println("Enter your choice");
+        Choice = input.nextInt();
+
+        while (Choice !=0){
+            switch (Choice) {
+                case 1:
+                    System.out.println("Draw the triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+
+        }
+    }
+}
